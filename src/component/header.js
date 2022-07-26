f04debb744e79563b563484d06c542988359c728
yusmardianto <yyusmar2@gmail.com>
2022-07-26 21:01:25 +0700

    fix repo

---
+++ src/component/header.js
@@ -0,0 +1,20 @@
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './header.css';
+import ImageHeader from '../img/header.png';
+import ImagePerson from '../img/Bitmap.png';
+import Button from 'react-bootstrap/Button';
+
+function Header() {
+  return (
+    <div className="headers">
+        <h1>WEEKEND FROM HOME</h1>
+        <p className="headerText">Stay active with a little workout.</p>
+        <img src={ImageHeader} className="imgHeader" />
+        <img src={ImagePerson} className="imgPerson" />
+        <button className="buttonHeader"> Let's Go </button>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/component/navbar.js
@@ -0,0 +1,25 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ImageNavbar from '../img/navbar.png';
+import './navbar.css';
+
+function NavbarHome() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={ImageNavbar} style={{width:"70%"}} />
+        </Navbar.Brand>
+        <p className="headertext1">Good Morning</p>
+        <p className="headertext2">Fellas</p>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarHome;
